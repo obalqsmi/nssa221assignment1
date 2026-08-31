3e0470a0c43c68ef85f0b47b9d3d86974ab40321
obalqsmi <971obaid@gmail.com>
2026-08-31 14:31:05 +0400

    Add shebang and make syshealth.sh executable

---
+++ syshealth.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# ===============================================
+# syshealth.sh - System Health & Log Analysis Toolkit
+# Lab 1 - Data Collector
+# Author: Obaid Qassim
+# Date: $(date +%Y-%m-%d)
+# ===============================================
